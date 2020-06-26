1a48970a6f9fdde0077b206145925ca39360f617
bronsinb <benyaminbronsin@gmail.com>
2020-06-25 17:08:58 -0700

    Worked on authentication and SQL

--- project1/application.py
+++ project1/application.py
@@ -1,14 +1,15 @@
 import os
+import requests
+import hashlib
 
-from flask import Flask, session
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-import requests
 
-key = "ev1BloPVJzToGutMhjI5g"
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": key, "isbns": "9781632168146"})
-print(res.json())
+#key = "ev1BloPVJzToGutMhjI5g"
+#res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": key, "isbns": "9781632168146"})
+#print(res.json())
 
 app = Flask(__name__)
 
@@ -25,7 +26,38 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+@app.route("/login")
+def login():
+    return render_template("auth.html", main="Log In", navs={"Register": "register"})
+
+@app.route("/register")
+def register():
+    return render_template("auth.html", main="Register", navs={"Log In": "login"})
+
+@app.route("/authenticate", methods=["POST"])
+def authenticate():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    auth_type = request.form.get("auth_type")
+
+    #Register
+    if auth_type is "Register":
+        db.execute("INSERT INTO flights (username, passhash) VALUES (:username, :passhash)",
+            {"username": username, "passhash": hashlib.md5(password.encode()).hexdigest()})
+        db.commit()
+
+    #Check if user exists
+    if db.execute("SELECT * FROM users WHERE username = :username AND passhash = :passhash", 
+        {"username": username, "passhash": hashlib.md5(password.encode()).hexdigest()}).rowcount == 0:
+        return redirect(url_for('login'))
+
+    session["username"] = username
+    return redirect(url_for('index'))
+    
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if session.get("username") is None:
+        return redirect(url_for('login'))
+    else:
+        return render_template("index.html", main="Home", navs=["Log Out"])
