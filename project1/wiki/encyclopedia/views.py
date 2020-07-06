ad189c1e772d02bded28fbeccd5fa1a5b94233c7
bronsinb <benyaminbronsin@gmail.com>
2020-07-06 13:47:40 -0700

    Added entry page and error page

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    content = util.get_entry(entry)
+
+    if content == None:
+        return render(request, "encyclopedia/error.html", {
+            "title": entry
+        })
+    html = markdown2.markdown(content)
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry,
+        "content": html
+    })
