16097a760f2cd790b5aaaa5d222bddbfcb9125d4
bronsinb <benyaminbronsin@gmail.com>
2020-07-08 13:40:14 -0700

    Added ability to add entries

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("random", views.rand, name="random"),
+    path("add", views.add, name="add"),
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,8 +1,15 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 import markdown2
 import random
 
 from . import util
+
+class AddEditForm(forms.Form):
+    title = forms.CharField()
+    entry = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': "Type Entry Here"}))
 
 def index(request):
     if request.GET:
@@ -18,7 +25,7 @@ def entry(request, entry):
 
     if content == None:
         return render(request, "encyclopedia/error.html", {
-            "title": entry
+            "error": f"'{entry}' Not Found"
         })
     html = markdown2.markdown(content)
     return render(request, "encyclopedia/entry.html", {
@@ -34,3 +41,23 @@ def rand(request):
         "title": title,
         "content": html
     })
+
+def add(request):
+    if request.method == "POST":
+        form = AddEditForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            entry = form.cleaned_data["entry"]
+            f=open(f"../wiki/entries/{title}.md","w+")
+            f.write(entry)
+            f.close()
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "encyclopedia/addedit.html", {
+                "form": form
+            })
+    else:
+        return render(request, "encyclopedia/addedit.html", {
+            "type": "Add",
+            "form": AddEditForm()
+        })
