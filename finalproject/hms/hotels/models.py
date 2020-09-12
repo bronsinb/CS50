129630b4bac4f338e542c9f974f1c8c5aa2af489
bronsinb <benyaminbronsin@gmail.com>
2020-09-12 15:34:44 -0700

    Added booking UI

--- finalproject/hms/hotels/models.py
+++ finalproject/hms/hotels/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
+#class Card(models.Model):
+ #   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cards")
+  #  number = models.CharField()
+
 class Hotel(models.Model):
     name = models.CharField(max_length=128)
     address = models.CharField(max_length=256)
@@ -15,6 +20,7 @@ class Hotel(models.Model):
 
 class Room(models.Model):
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name="rooms")
+    number = models.IntegerField(default=1)
     room_type = models.CharField(max_length=32, default="Single")
     image = models.CharField(max_length=2000, default="https://sciences.ucf.edu/psychology/wp-content/uploads/sites/63/2019/09/No-Image-Available.png")
     price = models.DecimalField(max_digits=8, decimal_places=2)
--- finalproject/hms/hotels/static/hotels/index.js
+++ finalproject/hms/hotels/static/hotels/index.js
@@ -8,6 +8,29 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll(".btn.btn-success.book").forEach((element) => {
         element.addEventListener('click', () => {
             document.getElementById("book").style.display = 'block';
+            document.getElementById("hotel-name").innerHTML = element.dataset.hotel;
+            document.getElementById("hotel-address").innerHTML = element.dataset.address;
+            
+            document.getElementById("room-type").innerHTML = "";
+            const roomtype = document.createElement('strong');
+            roomtype.innerHTML = "Type: ";
+            document.getElementById("room-type").append(roomtype);
+            document.getElementById("room-type").innerHTML += element.dataset.roomtype;
+            
+            document.getElementById("room-number").innerHTML = "";
+            const number = document.createElement('strong');
+            number.innerHTML = "Room Number: ";
+            document.getElementById("room-number").append(number);
+            document.getElementById("room-number").innerHTML += element.dataset.number;
+
+            document.getElementById("room-price").innerHTML = "";
+            const price = document.createElement('strong');
+            price.innerHTML = `$${element.dataset.price}`;
+            document.getElementById("room-price").append(price);
+            document.getElementById("room-price").innerHTML += " per Night";
+
+            document.getElementById("room-img").src = element.dataset.img;
+            document.getElementById("hotel-img").src = element.dataset.hotelimg;
         });
     });
 });
